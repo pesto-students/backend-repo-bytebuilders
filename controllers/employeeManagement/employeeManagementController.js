ac9f00f9627c9635d44ec10b7e7d6a59cd0315f6
mohantyDev <nihar.mohantydev@gmail.com>
2024-06-19 00:33:05 +0530

    who's on leave bug'

--- controllers/employeeManagement/employeeManagementController.js
+++ controllers/employeeManagement/employeeManagementController.js
@@ -53,7 +53,7 @@ const addEmployee = async (req, res) => {
     // Email options
     const mailOptions = {
       from: process.env.SMTP_USERNAME,
-      to: [req.user.email, "mnihar198@gmail.com"],
+      to: [req.body.email, "mnihar198@gmail.com"],
       subject: "Welcome to Our Company",
       html: htmlContent,
     };
--- controllers/miscellaneousFunctions/whoIsOnLeave.js
+++ controllers/miscellaneousFunctions/whoIsOnLeave.js
@@ -5,22 +5,45 @@ const queryEmployeesOnLeave = async (req, res) => {
   try {
     const userId = req.user._id;
     const user = await UserModel.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
     const organisationId = user.organisationUniqueId;
 
+    if (!organisationId) {
+      return res.status(400).json({ message: "Organisation ID not found for the user" });
+    }
+
     const currentDate = new Date();
+    currentDate.setHours(0, 0, 0, 0);
+    const nextDate = new Date(currentDate);
+    nextDate.setDate(currentDate.getDate() + 1);
+
+    console.log(`Current Date: ${currentDate}`);
+    console.log(`Next Date: ${nextDate}`);
+
     const leavesOnCurrentDate = await LeaveModel.find({
-      start_date: { $lte: currentDate },
+      start_date: { $lt: nextDate },
       end_date: { $gte: currentDate },
-      organisationUniqueId: organisationId, // Filter by organisation ID
+      organisationUniqueId: organisationId
     });
 
-    const uniqueUserIds = [
-      ...new Set(leavesOnCurrentDate.map((leave) => leave.user)),
-    ];
+    console.log(`Leaves on Current Date: ${JSON.stringify(leavesOnCurrentDate)}`);
+
+    if (leavesOnCurrentDate.length === 0) {
+      return res.status(200).json({ message: "No employees on leave today" });
+    }
+
+    const uniqueUserIds = [...new Set(leavesOnCurrentDate.map((leave) => leave.user.toString()))];
+    console.log(`Unique User IDs on Leave: ${uniqueUserIds}`);
 
     const employeesOnLeave = await UserModel.find({
-      _id: { $in: uniqueUserIds },
+      _id: { $in: uniqueUserIds }
     });
+
+    console.log(`Employees on Leave: ${JSON.stringify(employeesOnLeave)}`);
 
     return res.status(200).json(employeesOnLeave);
   } catch (error) {
