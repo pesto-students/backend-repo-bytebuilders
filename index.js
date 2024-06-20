8c44dec8665442b793794b0bc6e5428369e0112f
Nihar Ranjan Mohanty <mnihar198@gmail.com>
2024-06-20 15:39:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,27 @@ const cors = require("cors");
 const routes = require("./routes");
 const bodyParser = require("body-parser");
 const connectDB = require("./config/db");
+const https = require('https');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(cors({ origin: "https://master--jocular-fairy-66a01a.netlify.app/", credentials: true }));
+// Path to the SSL certificate and key files
+const sslOptions = {
+  key: fs.readFileSync('/etc/ssl/private/apache-selfsigned.key'),
+  cert: fs.readFileSync('/etc/ssl/certs/apache-selfsigned.crt')
+};
+
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(bodyParser.json());
 app.use("/api/", routes);
 
 connectDB()
   .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server is running on PORT: ${PORT}`);
+    // Create HTTPS server
+    https.createServer(sslOptions, app).listen(PORT, () => {
+      console.log(`HTTPS Server is running on PORT: ${PORT}`);
     });
   })
   .catch((err) => {
